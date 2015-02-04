1dbbb3420ed5b29f1ec2ac0a0711927f4eb13f63
Abraham Epton <abraham.epton@gmail.com>
2015-02-03 18:16:53 -0600

    Enabling downloading results and seeing status of recent jobs

--- bing_geocoder.py
+++ bing_geocoder.py
@@ -6,7 +6,7 @@ import requests
 import StringIO
 import time
 
-from csv import DictReader
+from csv import DictReader, DictWriter
 from datetime import datetime, timedelta
 
 LOGGER = logging.getLogger(__name__)
@@ -22,8 +22,12 @@ class BingGeocoder:
 
     def upload_addresses(self, addresses):
         """
-        Given list of dicts of {'address', 'entity_id'}, send them to Bing for processing.
+        Given list of dicts of {'address', 'entity_id'}, send them to Bing for processing. If
+        successful, returns string corresponding to job ID of uploaded batch.
         """
+        if not addresses or not len(addresses):
+            LOGGER.error('Unable to upload blank list of addresses to geocode')
+            return
         url = ('http://spatial.virtualearth.net/REST/v1/Dataflows/Geocode?input=csv&key=%s' %
                self.key)
         header_preamble = 'Bing Spatial Data Services, 2.0'
@@ -37,59 +41,137 @@ class BingGeocoder:
         ]
         header = '%s\n%s' % (header_preamble, ', '.join(header_fields))
         body = []
-        line_format = '%s,en-US,High,%s,,'
+        line_format = '%s,en-US,High,"%s",,'
         for address in addresses:
-            body.append(line_format % (address['entity_id'], address['address']))
+            body.append(line_format % (address['entity_id'], address['address'].replace('"', '\"')))
         data = "%s\n%s" % (header, '\n'.join(body))
         LOGGER.debug('Uploading %d addresses for geocoding' % len(body))
         try:
             r = requests.post(url, data=data, headers={"Content-Type": "text/plain"})
+            for rs in r.json()['resourceSets']:
+                for resource in rs['resources']:
+                    if 'id' in resource:
+                        LOGGER.debug('Successful upload; job id is %s' % resource['id'])
+                        return resource['id']
         except Exception, e:
             LOGGER.exception('Error uploading addresses: %s' % e)
 
-    def get_job_statuses(self, min_cutoff=1440):
+    def get_job_statuses(self, min_cutoff=4320, only_completed=False, job_id=''):
         """
         Connect to Bing API and get list of all available jobs. Only return jobs completed after
-        min_cutoff minutes ago; default is 24 hours' worth. Set to 0 to return all jobs.
+        min_cutoff minutes ago; default is 72 hours' worth. Set to 0 to return all jobs. Set
+        only_completed to True to see only completed jobs. If job_id is given, get_job_statuses
+        will return a 1-element list containing the resource for that job.
         """
         url = ('http://spatial.virtualearth.net/REST/v1/dataflows/listjobs?key=%s' % self.key)
         now = datetime.now(pytz.UTC)
         delta = timedelta(minutes=min_cutoff)
-        result_links = []
+        results = []
         r = requests.get(url)
         for rs in r.json()['resourceSets']:
             for resource in rs['resources']:
-                if 'completedDate' not in resource:
+                if job_id:
+                    if resource['id'] == job_id:
+                        return [resource]
+                if 'completedDate' not in resource and only_completed:
+                    # Job hasn't completed and we only want completed jobs
                     continue
-                completed = datetime.strptime(
-                    resource['completedDate'],
-                    '%a, %d %b %Y %H:%M:%S %Z')
-                if min_cutoff == 0 or completed.replace(tzinfo=pytz.UTC) > now - delta:
-                    for link in resource['links']:
-                        if 'name' in link and link['name'] == 'succeeded':
-                            result_links.append(link['url'])
-        return result_links
+                elif min_cutoff != 0:
+                    # We care about when the job was completed
+                    if 'completedDate' in resource:
+                        completed = datetime.strptime(
+                            resource['completedDate'],
+                            '%a, %d %b %Y %H:%M:%S %Z')
+                        if completed.replace(tzinfo=pytz.UTC) > now - delta:
+                            # Job was completed after min_cutoff minutes ago
+                            results.append(resource)
+                else:
+                    results.append(resource)
+        return results
 
-    def get_job_results(self, include_jobs):
+    def get_job_results(self, job_id):
         """
-        Connect to Bing API and get results of geocode jobs. If include_jobs is given, just return
-        results for those job ids. Return list of (entity_id, address, lat, lng) tuples for each job
+        Connect to Bing API and get results of geocode jobs. If job_id is given, just return
+        results for that job. Return list of (entity_id, address, lat, lng) tuples for each job
         id.
         """
-        result_links = get_job_statuses()
+        results = self.get_job_statuses(job_id=job_id)
         result_rows = []
-        for link in result_links:
-            if not include_jobs or link in include_jobs:
-                url = '%s?key=%s' % (link, settings.BING_MAPS_KEY)
-                r = requests.get(url, headers={"Content-Type": "text/plain"})
-                if len(r.text.splitlines()) > 2:
-                    result_data = StringIO.StringIO()
-                    for line in r.text.splitlines()[1:]:
-                        result_data.writeline(line)
-                    reader = DictReader(result_data)
-                    for line in reader:
-                        result_rows.append(line)
+        for result in results:
+            if result['status'] == 'Completed':
+                for link in result['links']:
+                    if link.get('name', '') == 'succeeded':
+                        url = '%s?key=%s' % (link['url'], self.key)
+                        r = requests.get(url, headers={"Content-Type": "text/plain"})
+                        if len(r.text.splitlines()) > 2:
+                            result_data = StringIO.StringIO()
+                            # Bing results data comes back with space+comma separator in header
+                            result_data.write('%s\n' % r.text.splitlines()[1].replace(', ', ','))
+                            for line in r.text.splitlines()[2:]:
+                                result_data.write('%s\n' % line)
+                            result_data.flush()
+                            result_data.seek(0)
+                            # Iterating twice over file in order to rely on csv DictReader parsing
+                            reader = DictReader(result_data)
+                            for line in reader:
+                                result_rows.append(line)
         return result_rows
+
+
+def get_addresses_from_file(path):
+    """
+    Given a path to a file, open it and read it into a list of dicts. There should be no header row,
+    and each line should be of the format: entity_id,address. Ideally, remove the commas from each
+    address, but if you don't the script will try to deal.
+    """
+    addresses = []
+    with open(path) as filehandle:
+        for line in filehandle:
+            data = line[:-1].split(',', 1)
+            if len(data) == 2 and data[0] and data[1]:
+                addresses.append({'entity_id': data[0], 'address': data[1]})
+    if not len(addresses):
+        print 'Heads up: not able to find any addresses in file %s' % path
+    return addresses
+
+
+def write_addresses_to_file(path, address_rows):
+    """
+    Given a path to a file, open it (creating it if it doesn't exist, overwriting if it does) and
+    write a CSV to the file containing the addresses and their lat/longs.
+    """
+    fieldnames = [
+        'Id',
+        'GeocodeRequest/Query',
+        'GeocodeResponse/Point/Latitude',
+        'GeocodeResponse/Point/Longitude'
+    ]
+    with open(path, 'w+') as filehandle:
+        writer = DictWriter(filehandle, fieldnames=fieldnames, extrasaction='ignore')
+        writer.writeheader()
+        for row in address_rows:
+            writer.writerow(row)
+
+
+def pretty_print_statuses(statuses):
+    """
+    Convenience method to print job status info comprehensibly.
+    """
+    for status in statuses:
+        print """
+        Job ID: %s
+        Current status: %s
+        Total entities: %s
+        Processed entities: %s
+        Failed entities: %s
+        -----------------------
+        """ % (
+            status['id'],
+            status['status'],
+            status['totalEntityCount'],
+            status['processedEntityCount'],
+            status['failedEntityCount']
+        )
 
 
 def main():
@@ -103,42 +185,35 @@ def main():
         else:
             print 'Error: Need to enter Bing Maps API key.'
             return
-    print geocoder.get_job_statuses(0)
+
+    option = raw_input('Type g to (g)et results, u to (u)pload addresses for geocoding, s to (s)ee'
+                       ' statuses of recent jobs: ')
+    if option.lower() not in ['g', 'u', 's']:
+        print 'Need to enter either g, s or u.'
+        return
+
+    if option.lower() == 'u':
+        path = raw_input('Enter path to file containing id,address pairs: ')
+        if not path:
+            print 'Need to provide a path to a file.'
+            return
+        job_id = geocoder.upload_addresses(get_addresses_from_file(path))
+        if job_id:
+            print 'Successful upload. Job id is %s' % job_id
+
+    if option.lower() == 'g':
+        job_id = raw_input('Enter job id to download results for, leave blank to see status of last'
+                           ' 24 hours\' worth of jobs: ')
+        results = geocoder.get_job_results(job_id)
+        if len(results):
+            path = raw_input('Enter path to file that will store geocoded addresses: ')
+            if not path:
+                print 'Need to provide a path to a file.'
+                return
+            write_addresses_to_file(path, results)
+
+    if option.lower() == 's':
+        pretty_print_statuses(geocoder.get_job_statuses())
 
 if __name__ == '__main__':
     main()
-
-
-"""
-class Command(BaseCommand):
-    help = "Geocodes any points that haven't been geocoded already."
-
-    def handle(self, *args, **options):
-        save_results_to_db()
-        LOGGER.info(
-            'Currently %d missing points' % PersonEntity.objects.filter(point=None).count())
-        begin = 0
-        end = 30000
-        seen = set()
-        while end < 300000:
-            # Our quota limit is 50 requests over 24 hours or 10 simultaneous jobs
-            LOGGER.info('Getting a new set, between %d and %d' % (begin, end))
-            people = PersonEntity.objects.filter(point=None)[begin:end]
-            addresses = []
-            for person in people:
-                address = create_address(
-                    person.address_1,
-                    person.address_2,
-                    person.city,
-                    person.state,
-                    person.zipcode).replace(',', ' ')
-                if address not in seen:
-                    seen.add(address)
-                    addresses.append({
-                        'address': address,
-                        'entity_id': 'personentity_%s' % person.pk
-                        })
-            upload_addresses(addresses)
-            begin = end
-            end += 30000
-"""
