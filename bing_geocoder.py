d955f75c772fb4cdf1518b0a6ff1b98a4ad7657e
Abraham Epton <abraham.epton@gmail.com>
2016-01-06 12:57:01 -0600

    Removing unnecessary fix

--- bing_geocoder.py
+++ bing_geocoder.py
@@ -114,8 +114,6 @@ class BingGeocoder:
                 address['address'] = line['address']
             elif 'Address' in line:
                 address['address'] = line['Address']
-            elif 'GeocodeRequest/Query' in line:
-                address['address'] = line['GeocodeRequest/Query']
             if 'id' in line:
                 address['entity_id'] = line['id']
             elif 'Id' in line:
